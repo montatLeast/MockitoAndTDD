5a85b279c08323909f7f727a1991ac2f6a031b67
montatLeast <314099319@qq.com>
2019-08-10 19:31:28 +0800

    test : write test case 3 compare every have two cards.

--- src/main/java/PokerCompare.java
+++ src/main/java/PokerCompare.java
@@ -1,9 +1,55 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class PokerCompare {
 
+    public static final String CARD_VALUE = "23456789TJQKA";
+    public static final String CARD_COLOR = "HCDS";
+    public static final String PLAYER_1_WIN = "Player 1 Win";
+    public static final String EQUAL = "Equal";
+    public static final String PLAYER_2_WIN = "Player 2 Win";
+
+
+
+    private Set<Integer> deskValue_1;
+    private Set<Integer> deskValue_2;
+
+
     public String compareCardGroup(String cardGroup_1, String cardGroup_2) {
-        int num1 = cardGroup_1.charAt(0);
-        int num2 = cardGroup_2.charAt(0);
-        return num1 > num2 ?  "Player 1 Win"
-                : ((num1 == num2) ? "Equal" : "Player 2 Win");
+        List<Card> desk_1 = initCardGroup(cardGroup_1);
+        List<Card> desk_2 = initCardGroup(cardGroup_2);
+        int result = compareTwoDesk(desk_1, desk_2);
+        return result==1 ?  PLAYER_1_WIN
+                : (result==0 ? EQUAL : PLAYER_2_WIN);
+    }
+
+    private List<Card> initCardGroup(String cardGroup_1) {
+        List<Card> desk = new ArrayList<>();
+        String[] cardGroup = cardGroup_1.split(" ");
+        for (String s : cardGroup){
+            Card card = new Card(CARD_VALUE.indexOf(s.charAt(0)),s.substring(1));
+            desk.add(card);
+        }
+        desk = desk.stream().sorted(Comparator.comparing(Card::getValue).reversed()).collect(Collectors.toList());
+        return desk;
+    }
+
+    private int compareTwoDesk(List<Card> desk_1, List<Card> desk_2){
+        deskValue_1 = desk_1.stream().map(Card::getValue).collect(Collectors.toSet());
+        deskValue_2 = desk_2.stream().map(Card::getValue).collect(Collectors.toSet());
+        return compareTwoDeskValue(deskValue_1, deskValue_2);
+    }
+
+    private int compareTwoDeskValue(Set<Integer> deskValue_1, Set<Integer> deskValue_2){
+        Set<Integer> allValue = new HashSet<>(deskValue_1);
+        Set<Integer> removeValue = new HashSet<>(deskValue_1);
+        allValue.addAll(deskValue_2);
+        removeValue.removeAll(deskValue_2);
+        allValue.removeAll(removeValue);
+        if (allValue.isEmpty()){
+            return 0;
+        }
+        Integer max = allValue.stream().max(Integer::compareTo).get();
+        return deskValue_1.contains(max) ? 1 : -1;
     }
 }
--- src/test/java/PokerCompareTest.java
+++ src/test/java/PokerCompareTest.java
@@ -15,10 +15,19 @@ public class PokerCompareTest {
     @Test
     public void should_return_equal_when_input_two_card_value_same(){
         String cardGroup_1 = "2H";
-        String cardGroup_2 = "2D";
+        String cardGroup_2 = "2C";
         PokerCompare pokerCompare = new PokerCompare();
         String result = pokerCompare.compareCardGroup(cardGroup_1, cardGroup_2);
         Assert.assertEquals("Equal", result);
+    }
+
+    @Test
+    public void should_return_player_1_win_when_every_input_double_cards_and_high_card(){
+        String cardGroup_1 = "3H 4H";
+        String cardGroup_2 = "2C 4C";
+        PokerCompare pokerCompare = new PokerCompare();
+        String result = pokerCompare.compareCardGroup(cardGroup_1, cardGroup_2);
+        Assert.assertEquals("Player 1 Win", result);
     }
 
 }
