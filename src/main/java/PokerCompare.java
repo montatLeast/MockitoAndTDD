7166e73c6beeb3ef9d553a565d8d3cea9a2c23f5
montatLeast <314099319@qq.com>
2019-08-10 16:23:20 +0800

    test : write test case 1 compare high card in two cards.

---
+++ src/main/java/PokerCompare.java
@@ -0,0 +1,7 @@
+public class PokerCompare {
+
+    public String compareCardGroup(String cardGroup_1, String cardGroup_2) {
+        return null;
+    }
+
+}
---
+++ src/test/java/PokerCompareTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PokerCompareTest {
+
+    @Test
+    public void should_return_player_2_win_when_input_two_card_and_high_card(){
+        String cardGroup_1 = "2H";
+        String cardGroup_2 = "3H";
+        PokerCompare pokerCompare = new PokerCompare();
+        String result = pokerCompare.compareCardGroup(cardGroup_1, cardGroup_2);
+        Assert.assertEquals("Player 2 Win", result);
+    }
+
+}
